50c7a1e03acb9eb0c7d473b9cf3a9cd7c5f212cf
mag002 <31187152+mag002@users.noreply.github.com>
2018-10-25 19:53:59 +0700

    Bunny

--- script/search.js
+++ script/search.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
     $(".search-button").click(function(){
         $(".search-number").toggle();
+        $(".search-box").toggleClass("col-11");
+        $(".search-button i").toggleClass("color-btn-change");
     });    
+    $('#user-select').change(function(){
+        $("#widthTempOption").html($('#user-select option:selected').text());
+        $(this).width($("#selectTagWidth").width()); 
+    });
+    $('#price-select').change(function(){
+        $("#widthTempOption2").html($('#price-select option:selected').text());
+        $(this).width($("#selectTagWidth2").width()); 
+    });
+    $('#cash').selectpicker();
 });
